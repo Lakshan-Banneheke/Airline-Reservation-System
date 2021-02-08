e8165c7af9a800e9ba4f543047028a44a8baf2ca
sw <shashiniw@gmail.com>
2021-02-08 14:25:26 +0530

    slight improvement

--- models/Staff.js
+++ models/Staff.js
@@ -64,7 +64,7 @@ class Staff {
         console.log("inside staff modal");
         console.log(today_date);
 
-        const query = 'SELECT schedule_id,aircraft_id,departure_time_utc,arrival_time_utc,duration,origin,destination,(select public.get_flight_state(arrival_time_utc))as flight_state FROM flight_schedule LEFT OUTER JOIN route USING(route_id) WHERE (actual_arrival is NULL and arrival_date=$1) ORDER BY arrival_time_utc DESC';
+        const query = 'SELECT schedule_id,aircraft_id,departure_time_utc,arrival_time_utc,duration,origin,destination,(select public.get_flight_state(arrival_time_utc))as flight_state FROM flight_schedule LEFT OUTER JOIN route USING(route_id) WHERE (actual_arrival is NULL and arrival_date=$1) ORDER BY arrival_time_utc ASC';
         
         const result = await pool.query(query, [today_date]);
         
