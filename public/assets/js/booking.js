092ab6210996a4daa7b87667b2dd49300eb77d06
Lakshan <btlakshan@gmail.com>
2021-02-09 21:13:11 +0530

    Selecting passengers based on count - half done

---
+++ public/assets/js/booking.js
@@ -0,0 +1,72 @@
+let bookingForm = $('#bookingForm');
+
+
+let start ='<div class="passengerCount"> <div class="booking-registration-form"> <div class="booking-form-area"> <h3 class="title">Passenger information</h3>'
+let end = '</div></div></div>'
+
+let passengerinfo = '<br><div class="form-group row">' +
+                        '<label class="col-lg-4 col-form-label">Full Name of Passenger</label>' +
+                        '<div class="col-lg-8">' +
+                            '<input type="text" class="form-control">' +
+                        '</div>' +
+                    '</div>' +
+                    '<div class="form-group row">' +
+                        '<label class="col-lg-4 col-form-label">Passport Number</label>' +
+                        '<div class="col-lg-8">' +
+                            '<input type="text" class="form-control">' +
+                        '</div>' +
+                    '</div>' +
+                    '<div class="form-group row">' +
+                        '<label class="col-lg-4 col-form-label">Date of Birth</label>' +
+                        '<div class="col-lg-8">' +
+                            '<input type="date" class="form-control">' +
+                        '</div>' +
+                    '</div>' +
+                    '<div class="form-group row">' +
+                        '<label class="col-lg-4 col-form-label">Seat Number</label>' +
+                        '<div class="col-lg-8">' +
+                            '<select class="select-bar" id="seatNumber">' +
+
+                            '</select>' +
+                        '</div>' +
+                    '</div><hr>'
+
+
+
+let selected;
+let dropdown;
+$(".seat-check").click(function () {
+    selected = [];
+    dropdown = '';
+    $('.plane input:checked').each(function() {
+        selected.push($(this).data('seat'));
+    });
+
+    selected.forEach(function (item, index) {
+        let menuItem;
+        if (index === 0){
+            menuItem = '<option value="'+ item +'" selected>' + item + '</option>'
+
+        } else {
+            menuItem = '<option value="'+ item +'">' + item + '</option>'
+        }
+        dropdown += menuItem;
+    });
+
+    $('#seatNumber').each(function () {
+        $(this).html(dropdown);
+    })
+})
+
+
+$("#passengers").change(function() {
+    bookingForm.children('.passengerCount').remove();
+    let passengerList = '';
+    for(let i=0; i<this.value; i++){
+        passengerList += passengerinfo
+    }
+    bookingForm.append(start + passengerList + end);
+    $('#seatNumber').each(function () {
+        $(this).html(dropdown);
+    })
+});
