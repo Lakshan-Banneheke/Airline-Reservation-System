b4ed7faaca5df591c7e6cf18d2b0e1ad7bd758fb
dhaura <dhaurapathirana@gmail.com>
2021-02-17 17:53:59 +0530

    previous bookings display

--- routes/booking.js
+++ routes/booking.js
@@ -1,10 +1,13 @@
 const router = require('express').Router();
 const BookingController = require('../controllers/bookingController');
+const ifRegisteredCustomer = require('../middleware/ifRegisteredCustomer');
 
 router.post('/createBooking', BookingController.createBooking);
 router.post('/payment/cancel', BookingController.cancelPayment);
 router.post('/payment/success', BookingController.paymentSuccess);
 router.get('/payment', BookingController.getPayment);
+router.get('/previous-bookings', ifRegisteredCustomer, BookingController.getPreviousBookings);
+router.post('/previous', ifRegisteredCustomer, BookingController.getPreviousBooking);
 router.post('/', BookingController.getBooking);
 router.get('/', BookingController.getBooking);
 router.delete('/deleteBooking', BookingController.deleteBooking);
